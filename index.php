e9d7681a527179eb120e38fd6667e44a538ecf6a
blackpikin <black_pikin@yahoo.com>
2015-06-25 12:57:03 +0100

    creation of home page

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+// Created by Halsey Takang (skinofblackpikin)
+$name = "Halsey";
+require_once 'default.php'; 
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home</title>
+         <?php loadScripts()?>
+    </head>
+<body>     
+    <?php createHeader();?>
+    <div class="row">
+        <div class="col-xs-3">
+            
+        </div>
+        <div class="col-xs-6">
+            
+        </div>
+        <div class="col-xs-3">
+            
+        </div>
+    </div>
+    
+    <?php createFooter(); ?>
+</body>
+</html>
+
+
