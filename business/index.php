376d62a9fd1b7cc12c5e41cc8768281f08aa11af
Halsey Takang <h.takang@camoo.cm>
2015-06-26 14:40:07 +0100

    restructuring of directories Design of home page

---
+++ business/index.php
@@ -0,0 +1,29 @@
+<?php
+// Created by Halsey Takang (skinofblackpikin)
+
+require_once '../shared/default.php';
+$siteName = "SkinOfBlackpikin"
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $siteName ?> - Business</title>
+    <?php loadScripts()?>
+</head>
+<body>
+<?php createHeader();?>
+<div class="row">
+    <div class="col-xs-12 col-sm-4">
+
+    </div>
+    <div class="col-xs-12 col-sm-4">
+
+    </div>
+    <div class="col-xs-12 col-sm-4">
+
+    </div>
+</div>
+
+<?php createFooter(); ?>
+</body>
+</html>
